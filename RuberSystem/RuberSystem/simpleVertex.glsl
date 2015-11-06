decc52e618cb20e0f8208c3c9ccfe7745bd3c63b
scarfyeatsduck <stephen.morse.938@my.csun.edu>
2015-11-05 20:53:02 -0800

    Create simpleVertex.glsl
    Adding a missing shader function.

---
+++ RuberSystem/RuberSystem/simpleVertex.glsl
@@ -0,0 +1,23 @@
+/* 
+SimpleVertex.glsl
+
+Vertex shader with position, color, normal and ModelViewProject
+input and color output.
+
+Mike Barnes
+8/16/2014
+*/
+
+# version 330 core
+
+in vec4 vPosition;
+in vec4 vColor;
+in vec3 vNormal;  // not used
+
+uniform mat4 ModelViewProjection; // = projection * view * model
+out vec4 color;
+
+void main() {
+  color = vColor;
+  gl_Position = ModelViewProject * vPosition;
+  }
